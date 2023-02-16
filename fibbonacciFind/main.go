778809660c86b1a6559584ad1effc5dc3e31e44b
Minh Son <54775734+mhson281@users.noreply.github.com>
2023-02-15 22:15:14 -0600

    adding function to print out fibbonacci numbers

---
+++ fibbonacciFind/main.go
@@ -0,0 +1,29 @@
+package main
+
+import ("fmt" ; "os"; "strconv")
+
+func findFib(n int) int {
+    if n == 0 || n == 1 {
+        return 1
+    } 
+
+    return findFib(n - 1) + findFib(n - 2)
+}
+
+func main() {
+
+    if len(os.Args) != 2 {
+        fmt.Println("Please enter the number of fibbonacci numbers to return")
+        return
+    }
+
+    n, err := strconv.Atoi(os.Args[1])
+    if err != nil {
+        fmt.Println("Please enter a number")
+        return
+    }
+    
+    for i := 0; i <= n; i++ {
+        fmt.Print(findFib(i), " ")
+    }
+}
